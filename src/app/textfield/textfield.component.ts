0bbdd3ad9b135a391daf921baca0d6e2265d0f5a
Dragonstb <Dragonstb@users.noreply.github.com>
2025-11-04 18:05:59 +0100

    Added accessibiliy features to the textfield

--- src/app/textfield/textfield.component.ts
+++ src/app/textfield/textfield.component.ts
@@ -7,12 +7,15 @@ import { Component, ElementRef, output, ViewChild } from "@angular/core";
 })
 export class GnericTextfield {
 
-    id: string = "abc";
+    id: string = "comp-01-01";
+    fullId: string = "Textfield-"+this.id;
     rows: number = 10;
     textEditable: boolean = true;
     deleteTextfieldEvent = output<string>();
     @ViewChild('textPanel', {static: true}) textPanel!: ElementRef<HTMLTextAreaElement>;
     @ViewChild('editPanel', {static: true}) editPanel!: ElementRef<HTMLDivElement>;
+    @ViewChild('fieldSet', {static: true}) fieldSet!: ElementRef<HTMLFieldSetElement>;
+    @ViewChild('legend', {static: true}) legend!: ElementRef<HTMLLegendElement>;
 
     addRow() {
         ++this.rows;
@@ -28,9 +31,13 @@ export class GnericTextfield {
         this.textEditable = editable;
         if(editable) {
             this.editPanel.nativeElement.classList.remove('hidden');
+            this.legend.nativeElement.classList.remove('hidden');
+            this.fieldSet.nativeElement.classList.add('editable');
         }
         else {
             this.editPanel.nativeElement.classList.add('hidden');
+            this.legend.nativeElement.classList.add('hidden');
+            this.fieldSet.nativeElement.classList.remove('editable');
         }
     }
 
