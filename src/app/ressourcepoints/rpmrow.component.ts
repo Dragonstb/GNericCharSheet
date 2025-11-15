60e7529076d0e8e4d4fd144737fb640188e55546
Dragonstb <Dragonstb@users.noreply.github.com>
2025-11-15 10:58:38 +0100

    Made RPM texts readonly when not in edit mode

--- src/app/ressourcepoints/rpmrow.component.ts
+++ src/app/ressourcepoints/rpmrow.component.ts
@@ -5,17 +5,19 @@ import { GNericCross3 } from "./cross3.component";
 import { GNericCross4 } from "./cross4.component";
 import { GNericCross5 } from "./cross5.component";
 import { GNericCross6 } from "./cross6.component";
-import { RPStatus } from "./rpstatus";
+import { GNericRPRowStats } from "./rprowstatus";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component ({
     selector: 'gneric-rpmrow',
-    imports: [GNericCross1, GNericCross2, GNericCross3, GNericCross4, GNericCross5, GNericCross6],
+    imports: [GNericCross1, GNericCross2, GNericCross3, GNericCross4, GNericCross5, GNericCross6, ReactiveFormsModule],
     templateUrl: './rpmrow.component.html',
     standalone: true
 })
 export class GNericRPMRow {
 
-    @Input() status: RPStatus[] = [];
+    @Input() status!: GNericRPRowStats;
     @Input() showText: boolean = true;
+    @Input() textEditable: boolean = true;
 
 }
