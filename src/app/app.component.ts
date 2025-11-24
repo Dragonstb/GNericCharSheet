fc2391430a76ed604c40f40b047b824ca563c4e4
Dragonstb <Dragonstb@users.noreply.github.com>
2025-11-24 18:40:44 +0100

    Made some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { GNericCheckboxList } from './checkboxes/checkboxes.component';
 
 @Component({
   selector: 'app-root',
-  imports: [GNericTable],
+  imports: [GnericTextfield, GNericTable, GNericRessourcePointsManager, GNericItemList, GNericCheckboxList],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
