e6d9af6d11e26924de8981c5c1eb07bd11b3a76a
Dragonstb <Dragonstb@users.noreply.github.com>
2026-01-18 18:42:12 +0100

    Implemented input validations for the sheet collection

--- src/app/sheetcollection/sheetcollectionmodel.ts
+++ src/app/sheetcollection/sheetcollectionmodel.ts
@@ -1,3 +1,5 @@
+import { ValidatorService } from "../../services/validator";
+import { ActionTypes } from "../ActionTypes";
 import { ElemTypes } from "../elemtypes";
 import { GNericSheetModel } from "../sheet/sheetmodel";
 
@@ -51,5 +53,103 @@ export class GNericSheetCollectionModel {
             type: ElemTypes.sheetcollection,
             content: sheetModels
         }
+    }
+
+    // _______________  update  _______________
+
+    updateModel(model: any): boolean {
+        if(!this.validateBaseModel(model)) {
+            return false;
+        }
+
+        let ok = false;
+        if(model.action === ActionTypes.collectionupdate) {
+            ok = this.updateCollection(model);
+        }
+        else if(model.action === ActionTypes.sheetupdate) {
+            if(model.hasOwnProperty('model') && model.model && typeof model.model === 'object') {
+                ok = this.updateSheet(model.model);
+            }
+        }
+
+        return ok;
+    }
+
+    updateCollection(model: any): boolean {
+        if(!this.validateCollectionLevelModel(model)) {
+            return false;
+        }
+
+        const newSheets: GNericSheetModel[] = [];
+        for (const sheet of model.content) {
+            const mod = {...sheet, action: ActionTypes.sheetupdate};
+            const newSheet = new GNericSheetModel(mod.id);
+            const ok = newSheet.updateModel(mod);
+            if(!ok) {
+                return false;
+            }
+            newSheets.push(newSheet);
+        }
+
+        this.sheets = newSheets;
+        return true;
+    }
+
+    updateSheet(model: any): boolean {
+        if(!ValidatorService.hasNonEmptyStringProperty('id', model)) {
+            return false;
+        }
+
+        for (const sheet of this.sheets) {
+            if(sheet.getId() === model.id) {
+                return sheet.updateModel(model);
+            }
+        }
+
+        return false;
+    }
+
+    // _______________  validate  _______________
+
+    validateBaseModel(model: any): boolean {
+        if(!ValidatorService.isModel(model)) {
+            return false;
+        }
+
+        if(!ValidatorService.hasNonEmptyStringProperty('type', model)) {
+            return false;
+        }
+
+        if(model.type !== ElemTypes.sheetcollection) {
+            return false;
+        }
+
+        if(!ValidatorService.hasNonEmptyStringProperty('action', model)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    validateCollectionLevelModel(model: any): boolean {
+        if(!model.hasOwnProperty('content') || !Array.isArray(model.content)) {
+            return false;
+        }
+
+        const idsInUse: Set<string> = new Set();
+        for (const sheet of model.content) {
+            if(typeof sheet !== 'object') {
+                return false;
+            }
+            if(!ValidatorService.hasNonEmptyStringProperty('id', sheet)) {
+                return false;
+            }
+            if(idsInUse.has(sheet.id)) {
+                return false;
+            }
+            idsInUse.add(sheet.id);
+        }
+
+        return true;
     }
 }
