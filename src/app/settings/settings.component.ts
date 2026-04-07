62cf188a709d848a08fd0d1be63b90ac29007dbc
Dragonstb <Dragonstb@users.noreply.github.com>
2026-04-07 17:15:30 +0200

    Implemented the download of the sheets and the compendium

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -14,6 +14,25 @@ export class GNericSettings {
     @Input() sheets: GNericSheetCollectionModel = new GNericSheetCollectionModel();
     compService = inject(CompendiumService);
 
+    downloadLink: HTMLAnchorElement | null = null;
+
+    downloadCompendium(): void {
+        const compModel: GNericCompendiumModel = this.compService.getCompendium();
+        
+        const json = {
+            compendium: compModel.getModel()
+        }
+
+        this.executeDownload(json, 'ttrpg-compendium.json');
+    }
+
+    downloadSheets(): void {
+        const json = {
+            sheets: this.sheets.getModel(),
+        }
+        this.executeDownload(json, 'ttrpg-sheets.json');
+    }
+
     downloadData(): void {
         const compModel: GNericCompendiumModel = this.compService.getCompendium();
         
@@ -22,7 +41,23 @@ export class GNericSettings {
             compendium: compModel.getModel()
         }
 
+        this.executeDownload(json, 'ttrpg-data.json');
+    }
+
+    private executeDownload(json: Object, filename: String): void {
         const output = JSON.stringify(json, null, 2);
-        console.dir(output);
+
+        const blob = new Blob([output], {type: 'application/json'});
+        const url = window.URL.createObjectURL(blob);
+        console.dir(url);
+
+        if(!this.downloadLink) {
+            this.downloadLink = document.createElement('a');
+        }
+        this.downloadLink.setAttribute('href', url);
+        this.downloadLink.setAttribute('download', String(filename));
+        this.downloadLink.dispatchEvent(new MouseEvent('click'));
+
+        window.URL.revokeObjectURL(url);
     }
 }
